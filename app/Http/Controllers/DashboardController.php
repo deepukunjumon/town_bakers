08f56115cbeeffb03eed9f8cd281c4a44d373a25
Deepu Kunjumon <deepukunjumon595@gmail.com>
2025-05-10 08:18:45 +0530

    Dasgboard stats for branch

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class DashboardController extends Controller
 {
@@ -33,10 +34,23 @@ class DashboardController extends Controller
             ->where('branch_id', Auth::user()->branch_id)
             ->count();
 
+        $pendingOrdersCount = DB::table('orders')
+            ->where('status', ORDER_STATUSES['pending'])
+            ->where('branch_id', Auth::user()->branch_id)
+            ->count();
+
+        $todaysPendingOrdersCount = DB::table('orders')
+            ->where('status', ORDER_STATUSES['pending'])
+            ->where('branch_id', Auth::user()->branch_id)
+            ->whereDate('delivery_date', Carbon::today())
+            ->count();
+
         return response()->json([
             'success' => true,
             'data' => [
                 'active_employees_count' => $activeEmployeesCount,
+                'pending_orders_count' => $pendingOrdersCount,
+                'todays_pending_orders_count' => $todaysPendingOrdersCount,
             ]
         ]);
     }
