33476b0ba68371118723278ee4758da287b7fe2a
Deepu Kunjumon <deepukunjumon595@gmail.com>
2025-05-11 16:59:51 +0530

    fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-require_once '../app/constants.php';
+require_once __DIR__ . '/../app/constants.php';
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
