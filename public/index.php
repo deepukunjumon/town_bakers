9e7444ca838593ea693a8240d905822cb5626f82
Deepu Kunjumon <deepukunjumon595@gmail.com>
2025-06-02 20:49:25 +0530

    CORS fix 3

--- public/index.php
+++ public/index.php
@@ -1,20 +1,12 @@
 <?php
-$allowedOrigins = ['*'];
-
-$origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
-if (in_array($origin, $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: $origin");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization');
-}
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
     exit;
 }
-
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
